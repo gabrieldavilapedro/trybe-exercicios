9bcd1876417037b91743e80c1fa57ebb052a303e
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-05-31 10:51:21 -0300

    exercicio 1

---
+++ trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-01-Primeiros-passos-no-JavaScript/exercicio1.js
@@ -0,0 +1,13 @@
+let num1 = 5
+let num2 = 10
+
+let Adicao = num1 + num2
+let Subtração = num1 - num2
+let Multiplicacao = num1 * num2
+let Divisao = num1 / num2
+let Modulo = num1 % num2
+
+console.log(Adicao);
+console.log(Subtração);
+console.log(Multiplicacao);
+console.log(Divisao);console.log(Modulo);
