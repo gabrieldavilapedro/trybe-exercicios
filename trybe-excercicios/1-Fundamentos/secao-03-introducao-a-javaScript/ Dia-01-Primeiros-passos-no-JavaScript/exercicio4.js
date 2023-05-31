8e207c6392a61df0e9ff83e294add6e2447c1b58
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-05-31 11:28:41 -0300

    exercicio4

---
+++ trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-01-Primeiros-passos-no-JavaScript/exercicio4.js
@@ -0,0 +1,11 @@
+let angulo1 = 40;
+let angulo2 = 65;
+let angulo3 = 75;
+let Triangulo = null
+
+if(angulo1 + angulo2 +angulo3 ===180){
+    Triangulo = true
+}else{
+    Triangulo = "erro"
+}
+console.log(Triangulo)
