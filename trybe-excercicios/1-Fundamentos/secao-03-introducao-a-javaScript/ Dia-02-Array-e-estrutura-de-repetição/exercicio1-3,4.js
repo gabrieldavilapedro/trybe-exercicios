23f579e4ee2fc1ddcc56202772f1cf462ba9d136
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-06-02 10:21:02 -0300

    exercicio 5

--- trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-02-Array-e-estrutura-de-repetição/exercicio1-3,4.js
+++ trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-02-Array-e-estrutura-de-repetição/exercicio1-3,4.js
@@ -13,6 +13,3 @@ if(mediaAritimetica > 20){
     console.log("O valor da média aritmética é menor ou igual a 20");
 }
 
-
-// Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
-
