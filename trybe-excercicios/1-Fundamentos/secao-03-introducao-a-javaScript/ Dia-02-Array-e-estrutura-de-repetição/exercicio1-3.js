d746ce0d78f2b8031037a9fd616a244f6fdac433
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-06-02 09:29:28 -0300

    exercicios 1.3 e 1.4

--- trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-02-Array-e-estrutura-de-repetição/exercicio1-3.js
+++ trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-02-Array-e-estrutura-de-repetição/exercicio1-3.js
@@ -1,7 +1,18 @@
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let SomaDosNumbesr = null; 
+let mediaAritimetica = null;
 
 for(let index = 0; index < numbers.length; index += 1){
     SomaDosNumbesr = SomaDosNumbesr + numbers[index]
  }   
-console.log(SomaDosNumbesr / numbers.length)
+mediaAritimetica = (SomaDosNumbesr / numbers.length)
+
+if(mediaAritimetica > 20){
+    console.log("O valor da média aritmética é maior que 20");
+}else {
+    console.log("O valor da média aritmética é menor ou igual a 20");
+}
+
+
+// Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que: caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”; e, caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+
