a57f6337bea95f076c9adf9c15030a192e84fefa
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-06-02 08:57:04 -0300

    somando numeros do array, exercicio1.2

---
+++ trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/ Dia-02-Array-e-estrutura-de-repetição/exercicio1.2.js
@@ -0,0 +1,7 @@
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let SomaDosNumbesr = null; 
+
+for(let index = 0; index < numbers.length; index += 1){
+    SomaDosNumbesr = SomaDosNumbesr + numbers[index]
+ }   
+console.log(SomaDosNumbesr)
