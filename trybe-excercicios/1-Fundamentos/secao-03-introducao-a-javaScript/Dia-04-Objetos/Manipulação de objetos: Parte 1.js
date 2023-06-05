ce127ae4ab4cc2266d524fe6e910c4c4ae6408b7
Gabriel D'avila Pedro <gabrieldavilapedroo@gmail.com>
2023-06-05 18:33:06 -0300

    parte 1

---
+++ trybe-excercicios/1-Fundamentos/secao-03-introducao-a-javaScript/Dia-04-Objetos/Manipulação de objetos: Parte 1.js
@@ -0,0 +1,14 @@
+const reader = {
+    name: 'Julia',
+    lastName: 'Pessoa',
+    age: 21,
+    favoriteBooks: [
+      {
+        title: 'O Senhor dos Anéis - a Sociedade do Anel',
+        author: 'J. R. R. Tolkien',
+        publisher: 'Martins Fontes',
+      },
+    ],
+  };
+
+console.log(`O livro favorito de ${reader.name} ${reader.lastName} se chama ${reader.favoriteBooks[0].title}`);
